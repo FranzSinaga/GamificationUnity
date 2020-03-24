b6aba63d3c5a6aee56ddd54c10b32d31d76c9884
FranzSinaga <sinagafranz12@gmail.com>
2020-03-24 09:13:21 +0700

    Add repository and create some page in gamification app

---
+++ Scripts/PopulateGrid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateGrid : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numberToCreate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        populate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void populate(){
+        GameObject newObject;
+
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            newObject = (GameObject) Instantiate(prefab, transform);
+            newObject.GetComponent<Image>().color = Random.ColorHSV();
+        }
+    }
+}
