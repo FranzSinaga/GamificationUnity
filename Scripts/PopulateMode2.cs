85921e5eddc21e357522e88dbdc4b5e258dae643
FranzSinaga <sinagafranz12@gmail.com>
2020-03-29 21:35:14 +0700

    Fix layout

--- Scripts/PopulateMode2.cs
+++ Scripts/PopulateMode2.cs
@@ -11,13 +11,17 @@ public class PopulateMode2 : MonoBehaviour
     public bool controller;
     public GameObject Content;
     public GameObject prefab;
-    public int numberToCreate;
-    private static int position = 0;
+    public int numberToCreate = 5;
+
+    public GameObject buttonNext, buttonPrev;
+    
     public Sprite[] AnimalImages;
     public AudioClip[] audio;
+    
     public static List<Sprite> ListAnimalImages;
     public static List<AudioClip> ListAnimalAudio;
-    private GameObject newButton;
+    
+    private static int position = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,17 +36,30 @@ public class PopulateMode2 : MonoBehaviour
     // Update is called once per frame
     void Update()    
     {
-           
+        if (position == numberToCreate-1)
+        {
+            buttonNext.SetActive(false);
+            buttonPrev.SetActive(true);
+        }
+        else if(position == 0)
+        {
+            buttonNext.SetActive(true);
+            buttonPrev.SetActive(false);
+        }
+        else
+        {
+            buttonNext.SetActive(true);
+            buttonPrev.SetActive(true);
+        }
     }
 
     void populate(){
+        GameObject newButton;
         ListAnimalImages = new List<Sprite>();
         ListAnimalAudio = new List<AudioClip>();
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < numberToCreate; i++)
         {
             newButton = Instantiate(prefab);
-            //newButton.transform.SetParent(this.transform, false);
-            //newButton.GetComponent<Image>().color = Random.ColorHSV();
             newButton.GetComponent<Image>().sprite = AnimalImages[i];
             newButton.GetComponent<AudioSource>().clip = audio[i];
             ListAnimalImages.Add(AnimalImages[i]);
