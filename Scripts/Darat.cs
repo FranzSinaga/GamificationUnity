c28235a0c2af2f058406334ed4bd8c0e9f02d418
FranzSinaga <sinagafranz12@gmail.com>
2020-03-31 15:15:06 +0700

    Menambahkan fungsi drag and drop pada menu bermain lvl 1

---
+++ Scripts/Darat.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darat : MonoBehaviour
+{
+    [SerializeField] private Transform bearPlace;
+
+    private Vector2 initialPosition;
+
+    private float deltaX, deltaY;
+
+    public static bool locked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        deltaX = touchPos.x - transform.position.x;
+                        deltaY = touchPos.y - transform.position.y;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                    {
+                        transform.position = new Vector2(touchPos.x - deltaX, touchPos.y - deltaY);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - bearPlace.position.x) <= 0.5f && 
+                        Mathf.Abs(transform.position.y - bearPlace.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(bearPlace.position.x, bearPlace.position.y);
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(initialPosition.x, initialPosition.y);
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Scripts/GameControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    [SerializeField] private GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Darat.locked && Air.locked && Udara.locked)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
