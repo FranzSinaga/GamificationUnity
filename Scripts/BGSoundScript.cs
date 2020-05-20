9e2c074b886df2f910a40888298816a9052129ba
FranzSinaga <sinagafranz12@gmail.com>
2020-05-20 11:14:50 +0700

    Add background music

---
+++ Scripts/BGSoundScript.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class BGSoundScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static BGSoundScript instance = null;
+
+    public static BGSoundScript Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
